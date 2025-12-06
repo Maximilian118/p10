42668a1fd925efa962b46b53382a429e0e9b80d2
Maximilian118 <maxcrosby118@gmail.com>
2025-12-06 23:02:59 +0400

    p10 link fix

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import { GitHub } from '@mui/icons-material'
 const Footer: React.FC = () => (
   <footer>
     <h5>Maximilian Crosby</h5>
-    <a href="https://github.com/Maximilian118/p10-game" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/Maximilian118/p10" target="_blank" rel="noopener noreferrer">
       <GitHub/>
     </a>
   </footer>
