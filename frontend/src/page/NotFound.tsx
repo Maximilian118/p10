6778ae44651db91708c1af43e2244d85b243671b
Maximilian118 <maxcrosby118@gmail.com>
2026-02-01 20:06:31 +0100

    not found centred

--- frontend/src/page/NotFound.tsx
+++ frontend/src/page/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const NotFound: React.FC = () => (
   <div className="content-container">
-    <img alt="Page not found." src="https://i.imgflip.com/4aq4be.jpg" style={{ width: "80%" }}/>
+    <img alt="Page not found." src="https://i.imgflip.com/4aq4be.jpg" style={{ width: "80%", margin: "auto" }}/>
   </div>
 )
 
